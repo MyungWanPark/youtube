64e765e04d209aca4b1ac08f1676e85ca930d34c
parkmyungwan <mywanpark@gmail.com>
2023-02-19 12:50:06 +0900

    test: VideoDetail

--- src/pages/tests/VideoDetail.test.tsx
+++ src/pages/tests/VideoDetail.test.tsx
@@ -1,22 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
 import { withRouter } from '../../test/util';
 import VideoDetail from '../VideoDetail';
-import { render } from '@testing-library/react';
 import { videoData } from './../../test/videoData';
 import ChannelInfo from './../../components/ChannelInfo';
 import RelatedVideos from '../../components/RelatedVideos';
-import renderer from 'react-test-renderer';
 
 jest.mock('../../components/ChannelInfo');
 jest.mock('../../components/RelatedVideos');
 
 describe('VideoDetail', () => {
-    /* afterEach(() => {
+    afterEach(() => {
         (ChannelInfo as jest.Mock).mockReset();
         (RelatedVideos as jest.Mock).mockReset();
-    }); */
+    });
 
     test('renders video item', () => {
-        const componet = renderer.create(
+        render(
             withRouter(
                 [
                     {
@@ -28,7 +29,12 @@ describe('VideoDetail', () => {
             )
         );
 
-        expect(componet.toJSON()).toMatchSnapshot();
         const { title, description, channelTitle, channelId } = videoData.snippet;
+        expect(screen.getByTitle(title)).toBeInTheDocument();
+        expect((ChannelInfo as jest.Mock).mock.calls[0][0]).toStrictEqual({
+            info: { id: channelId, title: channelTitle },
+        });
+        expect((RelatedVideos as jest.Mock).mock.calls[0][0]).toStrictEqual({ id: videoData.id });
+        expect(screen.getByText(description)).toBeInTheDocument();
     });
 });
