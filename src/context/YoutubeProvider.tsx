6a422f6680d3a76632b917c5115d8c1271339d90
parkmyungwan <mywanpark@gmail.com>
2023-03-23 15:46:35 +0900

    feat: use mock youtube data

--- src/context/YoutubeProvider.tsx
+++ src/context/YoutubeProvider.tsx
@@ -1,13 +1,13 @@
 import YoutubeImpl from '../api/youtube';
-import YoutubeClient from '../api/youtubeClient';
+// import YoutubeClient from '../api/youtubeClient';
 import { YoutubeApiContext } from './YoutubeApiContext';
-// import { YoutubeMockClient } from '../api/youtubeMockClient';
+import { YoutubeMockClient } from '../api/youtubeMockClient';
 
 type Children = {
     children: JSX.Element;
 };
-const client = new YoutubeClient();
-// const client = new YoutubeMockClient();
+// const client = new YoutubeClient();
+const client = new YoutubeMockClient();
 const youtube = new YoutubeImpl(client);
 
 export function YoutubeApiProvider({ children }: Children) {
